fa994fff4505352e16bb1066956c62f707631aa1
shurkeel1 <90396677+shurkeel1@users.noreply.github.com>
2021-09-14 10:09:11 -0500

    Add files via upload

---
+++ GitHub/GitHub/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GitHub
+//
+//  Created by AMStudent on 9/14/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Bull")
+            .padding()
+        Button(action: {}) {
+                Text("Hit ME")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ GitHub/GitHub/GitHubApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubApp.swift
+//  GitHub
+//
+//  Created by AMStudent on 9/14/21.
+//
+
+import SwiftUI
+
+@main
+struct GitHubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
